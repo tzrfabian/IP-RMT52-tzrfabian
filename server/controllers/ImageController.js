e93d8be3d554d5bbdf93ac42f0fc7ab53d85d5a1
Fabian Tenzara <fabiantenzara@gmail.com>
2024-09-04 17:53:57 +0700

    feat: add feature for upload image using imgur API

---
+++ server/controllers/ImageController.js
@@ -0,0 +1,38 @@
+const { Image }= require('../models');
+const imgur = require('imgur');
+const path = require('path');
+const axios = require('axios');
+
+class ImageController {
+    static async postImg(req, res, next) {
+        try {
+            if (!req.files || !req.files.sampleFile) {
+                throw { name: "NotFound", message: "No files were uploaded." };
+            }
+            let sampleFile = req.files.sampleFile;
+            // Buffer file to Base64
+            const base64Data = sampleFile.data.toString('base64');
+            // Upload file base64 to Imgur
+            const response = await axios.post('https://api.imgur.com/3/upload', {
+                image: base64Data,
+                type: 'base64'
+            }, {
+                headers: {
+                    Authorization: process.env.IMGUR_CLIENT_ID
+                }
+            });
+            const imgUrl = response.data.data.link;
+            const newImage = await Image.create({
+                imgName: sampleFile.name,
+                imgUrl: imgUrl,
+                userId: req.user.id
+            });
+
+            res.status(201).json(newImage);
+        } catch (err) {
+            next(err);
+        }
+    }
+}
+
+module.exports = ImageController;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
+const ImageController = require('../controllers/ImageController');
 const UserController = require('../controllers/UserController');
 const { authentication } = require('../middlewares/authentication');
 const errorHandler = require('../middlewares/errorHandler');
+const fileUpload = require('express-fileupload');
 const router = require('express').Router();
+
+router.use(fileUpload());
 
 router.get('/', (req, res) => {
     res.status(200).json({ message: "Hi Fellas" });
@@ -14,6 +18,9 @@ router.post('/login/google', UserController.googleLogin);
 
 // middleware
 router.use(authentication);
+
+router.get('/api/images');
+router.post('/api/images', ImageController.postImg);
 
 router.use(errorHandler)
 
